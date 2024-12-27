44ed6f673898c777d7cb59dfba2af2fa255b9377
vit <vitri.yanti@sisschools.org>
2024-12-27 14:15:33 +0700

    full Swag Labs Web testing project

---
+++ src/test/java/stepDef/Hooks.java
@@ -0,0 +1,28 @@
+package stepDef;
+
+import io.cucumber.java.*;
+
+import static helper.Utility.*;
+
+public class Hooks {
+
+    @BeforeAll
+    public static void setUp(){
+    }
+
+    @AfterAll
+    public static void tearDown(){
+    }
+
+    @Before
+    public void beforeTest(){
+        startDriver();
+    }
+
+    @After
+    public void afterTest() throws InterruptedException{
+        Thread.sleep(4000);
+        quitDriver();
+    }
+
+}
